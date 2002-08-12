83bf47895dc7aaf289ede5c8555abbb3709c213a
Kjetil Torgrim Homme <kjetil.homme@redpill-linpro.com>
2002-08-12 18:11:24 +0200

    use block I/O

--- stripnuls.c
+++ stripnuls.c
@@ -1,42 +1,69 @@
 #include <stdio.h>
 #include <math.h>
 
-#define BYTES_PER_SECOND (2*2*44100)
-static const int threshold = 1 * BYTES_PER_SECOND;
+static char nuls[4096];
+
+#define BLOCKS_PER_SECOND ((long) 2*2*44100/sizeof(nuls))
+static const int threshold = 1 * BLOCKS_PER_SECOND;
+static int bytecount = 0;
+
+int
+emit_nuls (int zeroes)
+{
+	int removed = 0;
+
+	if (zeroes > threshold) {
+		int newsize = 
+			sqrt (zeroes / BLOCKS_PER_SECOND)
+			* BLOCKS_PER_SECOND;
+		fprintf (stderr, "shrunk %d (%.1fs) into %d (%.1fs) at %.1fs\n",
+			 zeroes,
+			 zeroes * sizeof (nuls) / 2/2/44100.0,
+			 newsize,
+			 newsize * sizeof (nuls) / 2/2/44100.0,
+			 bytecount / 2/2/44100.0);
+		removed = zeroes - newsize;
+		zeroes = newsize;
+	}
+	
+	while (zeroes--)
+		write (1, nuls, sizeof (nuls));
+
+	return (removed);
+}
 
 int
 main (int argc, char **argv)
 {
-	int bytecount = 0;
 	int removed = 0;
 	int zeroes = 0;
-	int c;
+	int n;
+	int samples = sizeof (nuls) / sizeof (short);
+	signed short buf[samples];
 
-	while ((c = getchar ()) != EOF) {
-		++bytecount;
+	while ((n = read (0, buf, sizeof (buf)))) {
 
-		if (c == 0) {
-			if (zeroes)
-				++zeroes;
-			else if (bytecount % 4)
-				++zeroes;
-		} else if (zeroes) {
-			if (zeroes > threshold) {
-				int newsize = (int)
-					(sqrt (zeroes / BYTES_PER_SECOND)
-					 * BYTES_PER_SECOND / 4)
-					* 4;
-				newsize += zeroes % 4;
-				fprintf (stderr, "shrunk %d into %d\n",
-					 zeroes, newsize);
-				removed += zeroes - newsize;
-				zeroes = newsize;
+		bytecount += n;
+
+		if (n == sizeof (buf)) {
+			int i = 0;
+			while (i < samples &&
+			       buf[i] > -128 && buf[i] < 128)
+				++i;
+			if (i == samples) {
+				zeroes++;
+				continue;
 			}
-
-			while (zeroes--)
-				putchar (0);
 		}
-		if (! zeroes)
-			putchar (c);
+		if (zeroes) {
+			removed += emit_nuls (zeroes);
+			zeroes = 0;
+		}
+
+		write (1, buf, sizeof (buf));
 	}
+	if (zeroes)
+		removed += emit_nuls (zeroes);
+
+	return (0);
 }
