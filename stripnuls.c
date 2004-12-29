c0b942ae8d87d39c31925dcdc63fcada64e7056d
Kjetil Torgrim Homme <kjetil.homme@redpill-linpro.com>
2004-12-29 03:19:30 +0100

    add verbose mode

--- stripnuls.c
+++ stripnuls.c
@@ -4,8 +4,11 @@
 static char nuls[4096];
 
 #define BLOCKS_PER_SECOND ((long) 2*2*44100/sizeof(nuls))
-static const int threshold = 1 * BLOCKS_PER_SECOND;
+static const int threshold = 1 * BLOCKS_PER_SECOND + 2;
 static int bytecount = 0;
+static int in;
+static int out;
+static int verbose;
 
 int
 emit_nuls (int zeroes)
@@ -13,21 +16,26 @@ emit_nuls (int zeroes)
 	int removed = 0;
 
 	if (zeroes > threshold) {
-		int newsize = 
-			sqrt (zeroes / BLOCKS_PER_SECOND)
-			* BLOCKS_PER_SECOND;
-		fprintf (stderr, "shrunk %d (%.1fs) into %d (%.1fs) at %.1fs\n",
-			 zeroes,
-			 zeroes * sizeof (nuls) / 2/2/44100.0,
-			 newsize,
-			 newsize * sizeof (nuls) / 2/2/44100.0,
-			 bytecount / 2/2/44100.0);
+		int newsize = sqrt (zeroes / BLOCKS_PER_SECOND)
+			      * BLOCKS_PER_SECOND;
+		fprintf (stdout, "%d:%d@%d\n",
+			 zeroes, newsize, bytecount / sizeof (nuls));
+		if (verbose)
+			fprintf (stderr,
+				 "shrunk %.1fs into %.1fs at %.1fs\n",
+				 zeroes * sizeof (nuls) / 2/2/44100.0,
+				 newsize * sizeof (nuls) / 2/2/44100.0,
+				 bytecount / 2/2/44100.0);
 		removed = zeroes - newsize;
 		zeroes = newsize;
 	}
-	
-	while (zeroes--)
-		write (1, nuls, sizeof (nuls));
+
+	while (zeroes--) {
+		if (write (out, nuls, sizeof (nuls)) != sizeof (nuls)) {
+			perror ("write");
+			exit (2);
+		}
+	}
 
 	return (removed);
 }
@@ -38,10 +46,34 @@ main (int argc, char **argv)
 	int removed = 0;
 	int zeroes = 0;
 	int n;
+	int in;
 	int samples = sizeof (nuls) / sizeof (short);
 	signed short buf[samples];
+	const char *progname = argv[0];
 
-	while ((n = read (0, buf, sizeof (buf)))) {
+	if (argc > 1 && strcmp (argv[1], "-v") == 0) {
+		verbose = 1;
+		++argv; --argc;
+	}
+
+	if (argc != 3) {
+		fprintf (stderr, "Usage: %s [-v] infile outfile\n",
+			 progname);
+		exit (64);
+	}
+	in = open (argv[1], O_RDONLY);
+	if (in == -1) {
+		perror(argv[1]);
+		exit (1);
+	}
+
+	out = open (argv[2], O_CREAT | O_WRONLY);
+	if (out == -1) {
+		perror(argv[2]);
+		exit (1);
+	}
+
+	while ((n = read (in, buf, sizeof (buf)))) {
 
 		bytecount += n;
 
@@ -60,7 +92,10 @@ main (int argc, char **argv)
 			zeroes = 0;
 		}
 
-		write (1, buf, sizeof (buf));
+		if (write (out, buf, n) != n) {
+			perror(argv[2]);
+			exit (2);
+		}
 	}
 	if (zeroes)
 		removed += emit_nuls (zeroes);
